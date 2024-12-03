5d8a529be299f4c7160c93e772e0ea3506e86f79
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2024-12-03 09:38:16 +0100

    Major code upgrade 3
    - move graphics into output folder
    - create mesh checker file

--- src/F_MeshChecker.py
+++ src/F_MeshChecker.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec  2 19:59:19 2024
-
-@author: GEr
+Author: Dr. Georg H. Erharter - georg.erharter@ngi.no
+Script that performs different checks of the generated meshes.
 """
 
 import trimesh
 from os import listdir
 
+# get ids of all generated meshes
 ids = [c.split('_')[0] for c in listdir(r'../combinations') if 'discontinuities' in c]
 
-erroneous_meshes = ['243751266175', '769078262648', '926793301448']
+# check if meshes are too large or too small (must be within 10x10x10 cube)
+erroneous_meshes = []
 
 for id_ in ids:
     if id_ in erroneous_meshes:
@@ -24,5 +25,5 @@ for id_ in ids:
         print(id_, round(small_side, 3), round(large_side, 3))
         if small_side < 10:
             raise(ValueError(f'{id_} too small'))
-        if large_side > 11:
+        if large_side > 10.2:
             raise(ValueError(f'{id_} too large'))
